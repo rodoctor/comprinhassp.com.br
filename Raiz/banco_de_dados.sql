6af6cbabb850a37d5ed260e755df48db194fe976
Rodolfo Santos <rodolfomoreira88@gmail.com>
2017-03-10 00:44:07 -0300

    Delete

--- Raiz/banco_de_dados.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE cadastros (
-ID int NOT NULL AUTO_INCREMENT,
-Nome varchar(255) Not Null,
-Sobrenome varchar(255) Not Null,
-Telefone varchar(255) Not Null,
-Email varchar(255) Not Null,
-PRIMARY KEY (ID)
-)
--- Raiz/conecta_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = "mysql.hostinger.com.br";
-$banco = "u902202453_sp";
-$usuario = "u902202453_sp";
-$senha = "u902202453_sp";
-
-$conecta = new mysqli($hostname, $usuario, $senha, $banco);
-?>
